733391b9768f06b38bc83013ccbb315a867b2201
xperexmax <xperexmax@users.noreply.github.com>
2018-10-10 11:47:31 +0300

    hw to 4
    w

---
+++ lession4/homework/homework/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoursLecture
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] input = {
+                {"John and Mary have a discussion","1"},
+                {"John and Mary interview","3"},
+                {"F2F","4"},
+                {"General meeting","7"}
+            };
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        if (input[i, j].Length < 10)
+                            Console.Write($"{input[i, j].ToLower()} ");
+                        else
+                            Console.Write($"{input[i, j].ToLower().Substring(0, 10)} \n");
+                    }
+
+                    else
+                        Console.Write((days)int.Parse(input[i, j]));
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        enum days
+        {
+            monday = 1,
+            tuesday = 2,
+            wednesday = 3,
+            thursday = 4,
+            friday = 5,
+            saturday = 6,
+            sunday = 7
+        }
+    }
+}
