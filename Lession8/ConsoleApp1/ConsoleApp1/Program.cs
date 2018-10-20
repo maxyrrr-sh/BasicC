a8d0980fde1f8064fc81214652a3197d0e35e56a
xperexmax <xperexmax@users.noreply.github.com>
2018-10-20 10:52:35 +0300

    Update Lession8/ConsoleApp1/ConsoleApp1/Program.cs

--- Lession8/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lession8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,16 +6,45 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    internal class Weapon
+    {
+        public byte damage { get; set; }
+        public byte weight { get;  set; }
+
+        public Weapon(byte damage, byte weight)
+        {
+            this.weight = weight;
+            this.damage = damage; 
+        }
+        Weapon katana = new Weapon(80, 20);
+        Weapon nunchaks = new Weapon(60, 10);
+        Weapon stick = new Weapon(50, 40);
+        Weapon hammer = new Weapon(90, 80);
+    }
+    internal class Ninja : Weapon
+    {
+        bool isInvifible = false;
+        object weapon;
+        public Ninja()
+        {
+            
+        }
+
+        void attack()
+        {
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+           
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.ReadKey();          
         }
     }
+
+    
 }
